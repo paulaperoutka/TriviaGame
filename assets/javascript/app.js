667cbbace2472f8db9bcb63dd45d70887a19405d
paulaperoutka <paulaperoutka@gmail.com>
2018-01-14 17:45:06 -0500

    favicon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -277,7 +277,7 @@ $(document).ready (
 		clearInterval(intervalId);
 		emptyQuestionAnswers ();
 		setTimeout(function () {
-			$("#messageColumn").html("Finito! Let's see your stats. <br>");
+			$("#messageColumn").html("Finito! Let's see your stats. <br><br>");
 			$("#messageColumn").append("You answered " + counters.correctAnswers + " questions correctly! <br><br>");
 			$("#messageColumn").append("You answered " + counters.incorrectAnswers + " questions incorrectly. <br><br>");
 			$("#messageColumn").append("You ran out of time on " + counters.timesUp + " questions. <br><br>");
